4b1f71d561686f12b755794e0de0a8cebaad174f
abtinmo <abtin@riseup.net>
2020-06-19 22:38:58 +0430

    fix season typo

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import wget
 
-url = "http://xyz.ij/Series/HomeWatch/S03/480p/HomeWatch.S{season}E{episode}.480p.mkv"
-first_season, last_season = 3, 8
+url = "http://xyz.ij/Series/HomeWatch/S{season}/480p/HomeWatch.S{season}E{episode}.480p.mkv"
+first_season, last_season = 5, 8
 episode_count = 12
 
 for season in range( first_season, last_season + 1):
     episode = 1
     while episode <= episode_count:
+        print(f"\nSeason: {season}\nepisode: {episode}\n")
         wget.download(
                 url.format(
                     season=str(season).rjust(2, "0"),
